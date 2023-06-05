f594f424e328ce350ba00f63054cd6a37c02a8d1
ravi <ravi@pop-os.localdomain>
2023-06-05 18:07:20 +0530

    satup models

--- backend/Music/models.py
+++ backend/Music/models.py
@@ -15,10 +15,26 @@ class BaseModel(models.Model):
 class Tag(BaseModel):
     tag = models.CharField(max_length=255, null=False, blank=False)
 
+    class Mata:
+        db_table = 'tags'
+        ordering = ['-id']
+        verbose_name = 'Tag'
+        verbose_name_plural = 'Tag of Music'
+    
+    def __str__(self):
+        return self.tag
 
 class Type(BaseModel):
     type_of_music = models.CharField(max_length=255, null=False, blank=False)
 
+    class Mata:
+        db_table = 'types'
+        ordering = ['-id']
+        verbose_name = 'Type'
+        verbose_name_plural = 'Type of Music'
+    
+    def __str__(self):
+        return self.type_of_music
 
 class Info(BaseModel):
     composer = models.TextField(max_length=255, null=True, blank=True)
@@ -26,6 +42,15 @@ class Info(BaseModel):
     songwriter = models.TextField(max_length=255, null=True, blank=True)
     used_by = models.TextField(max_length=255, null=True, blank=True)
 
+    class Mata:
+        db_table = 'infos'
+        ordering = ['-id']
+        verbose_name = 'Information'
+        verbose_name_plural = 'Information of Music'
+    
+    def __str__(self):
+        return f'{self.composer}-{self.lyricist}-{self.songwriter}'
+    
 
 class Music(BaseModel):
     name = models.CharField(max_length=255, null=False, blank=False)
@@ -33,6 +58,15 @@ class Music(BaseModel):
     thumbnail = models.URLField(max_length=500, default='https://alcoholproject.blob.core.windows.net/mankind/capture_image_2022-12-27_17:31:58_893082.png')
     music = models.URLField(max_length=500, null=False, blank=False)
     lyricis = models.URLField(max_length=500, null=True, blank=True)
-    info_id = models.ForeignKey(Info, on_delete=models.CASCADE)
+    info_id = models.OneToOneField(Info, on_delete=models.CASCADE)
     tag_id = models.ManyToManyField(Tag)
     type_of_music_id = models.ManyToManyField(Type)
+
+    class Mata:
+        db_table = 'musics'
+        ordering = ['-id']
+        verbose_name = 'Music'
+        verbose_name_plural = 'Music Library'
+    
+    def __str__(self):
+        return f'{self.name}'
