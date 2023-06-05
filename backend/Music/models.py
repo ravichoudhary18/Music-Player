3e8713784eb85aba6c51d07d7b1ee1d17aae42f7
ravi <ravi@pop-os.localdomain>
2023-06-05 17:56:27 +0530

    satup models

--- backend/Music/models.py
+++ backend/Music/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class BaseModel(models.Model):
+    id = models.AutoField(primary_key=True)
     created_on = models.DateTimeField(auto_now_add=True)
     created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='%(class)s_created_by')
     updated_on = models.DateTimeField(auto_now=True)
@@ -12,17 +13,14 @@ class BaseModel(models.Model):
 
 
 class Tag(BaseModel):
-    id = models.AutoField(primary_key=True)
     tag = models.CharField(max_length=255, null=False, blank=False)
 
 
 class Type(BaseModel):
-    id = models.AutoField(primary_key=True)
     type_of_music = models.CharField(max_length=255, null=False, blank=False)
 
 
 class Info(BaseModel):
-    id = models.AutoField(primary_key=True)
     composer = models.TextField(max_length=255, null=True, blank=True)
     lyricist = models.TextField(max_length=255, null=True, blank=True)
     songwriter = models.TextField(max_length=255, null=True, blank=True)
@@ -30,11 +28,11 @@ class Info(BaseModel):
 
 
 class Music(BaseModel):
-    id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=255, null=False, blank=False)
     description = models.CharField(max_length=255, null=False, blank=False)
     thumbnail = models.URLField(max_length=500, default='https://alcoholproject.blob.core.windows.net/mankind/capture_image_2022-12-27_17:31:58_893082.png')
     music = models.URLField(max_length=500, null=False, blank=False)
     lyricis = models.URLField(max_length=500, null=True, blank=True)
-    tag = models.ManyToManyField(Tag)
-    type_of_music = models.ManyToManyField(Type)
+    info_id = models.ForeignKey(Info, on_delete=models.CASCADE)
+    tag_id = models.ManyToManyField(Tag)
+    type_of_music_id = models.ManyToManyField(Type)
